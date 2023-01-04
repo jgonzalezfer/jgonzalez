816f4cc83ce747b15aad36d9ec2673c2860c7e71
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2023-01-04 11:45:09 -0300

    df

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Joel González F',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Joel González F. is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Joel González F',
+        short_name: 'Joel González F',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
